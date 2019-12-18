a63221a3929a56a919e1ef917cbfed94a3a10293
Jeffacy99 <mwaniajapheth1999@gmail.com>
2019-12-18 12:35:35 +0300

    api call

--- app/views.py
+++ app/views.py
@@ -1,8 +1,15 @@
+from .request import get_movies
+
+@app.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
 
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title,popular=popular_movies)
